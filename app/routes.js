678617447b0d96a8e7fab395f08c7640cee68194
Joey Buczek <joe.buczek@gmail.com>
2016-03-12 07:27:37 -0500

    added ejs templates

---
+++ app/routes.js
@@ -0,0 +1,46 @@
+module.exports = function(app, passport) {
+    
+    // HOME PAGE ================================
+    app.get('/', function(req, res){
+        res.render('index.ejs');
+    });
+    
+    // LOGIN PAGE ===============================
+    app.get('/login', function(req, res){
+        // pass in any flash messages
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+    
+    // app.post('/login', passport stuff goes here);
+    
+    // SIGNUP PAGE ==============================
+    app.get('/signup', function(req, res){
+        // pass in any flash messages
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+    
+    // app.post('/signup', passport stuff goes here);
+    
+    // PROFILE PAGE =============================
+    // this is protected, must be loggin in to view
+    app.get('/profile', isLoggedIn, function(req, res){
+        // pass in user
+        res.render('profile.ejs', { user: req.user });
+    });
+    
+    // LOGOUT ===================================
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+// middleware to make sure a user is logged in
+function isLoggedIn(req, res, next){
+    // are they authenticated?
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect('/');
+    }
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+// set up ======================================================================
+// modules and tools
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to the database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up express app
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+
+// passport requirements - use env.js for secret in this spot:
+app.use(session({ secret: 'dvoteyourself' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persist login sessions
+app.use(flash()); 
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load routes and pass in app/passport
+
+// 404 catch
+app.use(function(req, res){
+    res.send('404 - Sorry, unable to locate what you were looking for.');
+});
+
+// listen ======================================================================
+app.listen(port, function(){
+    console.log('Now listening on port ' + port);
+});
