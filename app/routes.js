36651a6e805e474d31289418deb34a2033f12573
Joey Buczek <joe.buczek@gmail.com>
2016-03-21 02:16:01 -0400

    chart.js can now access locally passed data

--- app/routes.js
+++ app/routes.js
@@ -36,12 +36,26 @@ var pollObj2 = {
     ],
     voters: ['username1','username2','ipaddress2']
 };
-var chartData = [{
-    value: 400,
-    color:"#F7464A",
-    highlight: "#FF5A5E",
-    label: "Red"
-}];
+var chartData = [
+    {
+        value: 2,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red Delicious"
+    },
+    {
+        value: 3,
+        color:"#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Gala"
+    },
+    {
+        value: 1,
+        color:"#FDB45C",
+        highlight: "#FFC870",
+        label: "Granny Smith"
+    }
+];
 
 
 // exports ======================================
--- public/js/pieChart.js
+++ public/js/pieChart.js
@@ -1,15 +1,28 @@
 var data = [
     {
-        value: 300,
+        value: 2,
         color:"#F7464A",
         highlight: "#FF5A5E",
-        label: "Red"
+        label: "Red Delicious"
+    },
+    {
+        value: 3,
+        color:"#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Gala"
+    },
+    {
+        value: 1,
+        color:"#FDB45C",
+        highlight: "#FFC870",
+        label: "Granny Smith"
     }
 ];
 var options = { 
     percentageInnerCutout : 50, 
     animateScale : false, 
-    animationEasing : "easeOutBounce" 
+    animationEasing : "easeOutQuart" 
 };
+var data2 = JSON.parse(document.getElementById('chartData').innerHTML);
 var ctx = document.getElementById('chartCanvas').getContext('2d');
-new Chart(ctx).Pie(data, options);
+new Chart(ctx).Pie(data2, options);
